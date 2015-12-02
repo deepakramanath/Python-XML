d74e967ed54182c0adbe5d82eaac6382895bb77c
deepak <deepak.narayan@gmail.com>
2015-12-02 17:44:11 +1100

    Editing changes

--- Python-XML.py
+++ Python-XML.py
@@ -27,46 +27,46 @@ print "\n"
 datawrite = open('XMLResults.txt', 'w+')
 
 for treeProperties in xmlTree.findall('.//'):
-	for prop in treeProperties.findall('properties/property'):
-		propName = prop.get('name')
-		propValue = prop.get('value')
-		if propName == 'startnumber' or propName == "team":
-			break
-		else:
-			print "%s: %s" % (propName, propValue)
-			datawrite.write('%s: %s' % (propName, propValue) + '\n')
+    for prop in treeProperties.findall('properties/property'):
+        propName = prop.get('name')
+        propValue = prop.get('value')
+        if propName == 'startnumber' or propName == "team":
+        break
+        else:
+            print "%s: %s" % (propName, propValue)
+            datawrite.write('%s: %s' % (propName, propValue) + '\n')
 		fastest = prop.findall('.//participant')
-		if fastest:
-			for f in fastest:
-				fastestPerson = f.get('name')
-				print "Fastest participant name:", fastestPerson
-				datawrite.write('Fastest participant name: %s' % (fastestPerson) + '\n')
+        if fastest:
+            for f in fastest:
+                fastestPerson = f.get('name')
+                print "Fastest participant name:", fastestPerson
+                datawrite.write('Fastest participant name: %s' % (fastestPerson) + '\n')
 
 print "\n**Event participants**\n"
 datawrite.write('\n*Event participants**\n')
 
 for treeEvent in xmlTree.findall('.//'):
-	for event in treeEvent.findall('event/event_participant'):
-		participantNumber = event.get('number')
-		participantName = event.find('participant').get('name')
-		participantGender = event.find('participant').get('gender')
+    for event in treeEvent.findall('event/event_participant'):
+        participantNumber = event.get('number')
+        participantName = event.find('participant').get('name')
+        participantGender = event.find('participant').get('gender')
 
-		print "Participant number: %s" % participantNumber
-		datawrite.write('Participant number: %s' % (participantNumber) + '\n')		
-		print "Name: %s, Gender: %s" % (participantName, participantGender)
-		datawrite.write('Name: %s, Gender: %s' % (participantName.encode('utf8'), participantGender) + '\n')
+        print "Participant number: %s" % participantNumber
+        datawrite.write('Participant number: %s' % (participantNumber) + '\n')		
+        print "Name: %s, Gender: %s" % (participantName, participantGender)
+        datawrite.write('Name: %s, Gender: %s' % (participantName.encode('utf8'), participantGender) + '\n')
 		
 		
-		results = event.findall('.//result')
-		if results:
-			for result in results:
-				resultName = result.get('result_code')
-				resultValue = result.get('value')
-				print "%s: %s" % (resultName, resultValue)
-				datawrite.write('%s: %s' % (resultName, resultValue) + '\n')
+        results = event.findall('.//result')
+        if results:
+            for result in results:
+                resultName = result.get('result_code')
+                resultValue = result.get('value')
+                print "%s: %s" % (resultName, resultValue)
+                datawrite.write('%s: %s' % (resultName, resultValue) + '\n')
 				
-		print "******************************************"
-		datawrite.write('******************************************' + '\n')
+        print "******************************************"
+        datawrite.write('******************************************' + '\n')
 
 datawrite.close()
 xmlFile.close()
